1c4347319bfb1f9f40c5eb6d657790c1f6e1c230
Laszlo Dobos <dobos@jhu.edu>
2024-05-30 12:02:23 -0400

    Run scripts in profiler mode.

--- python/pfs/ga/pipeline/scripts/script.py
+++ python/pfs/ga/pipeline/scripts/script.py
@@ -9,6 +9,7 @@ class Script():
 
     def __init__(self):
         self.__debug = False
+        self.__profile = False
         self.__log_level = logging.INFO
 
         self.__parser = ArgumentParser()
@@ -17,6 +18,11 @@ class Script():
         return self.__debug
     
     debug = property(__get_debug)
+
+    def __get_profile(self):
+        return self.__profile
+    
+    profile = property(__get_profile)
 
     def __get_log_level(self):
         return self.__log_level
@@ -39,10 +45,12 @@ class Script():
         
     def _add_args(self):
         self._add_arg('--debug', action='store_true', help='Enable debug mode')
+        self._add_arg('--profile', action='store_true', help='Enable performance profiler')
         self._add_arg('--log-level', type=str, help='Set log level')
 
     def _init_from_args(self, args):
         self.__debug = self._get_arg('debug', args, self.__debug)
+        self.__profile = self._get_arg('profile', args, self.__profile)
         self.__log_level = self._get_arg('log_level', args, self.__log_level)
         if isinstance(self.__log_level, str) and hasattr(logging, self.__log_level.upper()):
             self.__log_level = getattr(logging, self.__log_level.upper())
@@ -53,7 +61,32 @@ class Script():
         self._add_args()
         self.__parse_args()
         self._init_from_args(self.__args)
+
+        # NOTE: debugging is started from the wrapper shell script
+
+        if self.__profile:
+            import cProfile
+            import pstats
+            import io
+
+            pr = cProfile.Profile()
+            pr.enable()
+        else:
+            pr = None
+
         self.run()
+
+        if pr is not None:
+            pr.disable()
+
+            # Save profiler data to file
+            with open(os.path.join('profile.cum.stats'), 'w') as f:
+                ps = pstats.Stats(pr, stream=f).sort_stats('cumulative')
+                ps.print_stats()
+
+            with open(os.path.join('profile.tot.stats'), 'w') as f:
+                ps = pstats.Stats(pr, stream=f).sort_stats('time')
+                ps.print_stats()
 
     def run(self):
         raise NotImplementedError()
