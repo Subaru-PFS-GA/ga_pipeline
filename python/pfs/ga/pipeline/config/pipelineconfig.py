b0f42daac12ac1e881d48819a57883740b3a5b5a
Laszlo Dobos <dobos@jhu.edu>
2024-10-21 14:40:26 -0400

    Removed gapipe specific variable.

--- python/pfs/ga/pipeline/config/pipelineconfig.py
+++ python/pfs/ga/pipeline/config/pipelineconfig.py
@@ -7,10 +7,9 @@ class PipelineConfig(Config):
         self.trace_args = {}
 
         self.workdir = self._get_env('GAPIPE_WORKDIR')        # Working directory, must be writable         
-        self.datadir = self._get_env('GAPIPE_DATADIR')        # PFS survey data directory root
+        self.outdir = self._get_env('GAPIPE_WORKDIR')         # Directory for output data files, must be writable
         self.logdir = None                                    # Log directory, must be writable
         self.figdir = None                                    # Figure directory, must be writable
-        self.outdir = self._get_env('GAPIPE_WORKDIR')         # Directory for output data files, must be writable
 
         self.loglevel = logging.INFO
 
