3f94755179685f103b4880e664154aef371b02d3
Laszlo Dobos <dobos@jhu.edu>
2024-10-22 15:58:57 -0400

    Moved PFS data repo browser to pfsspec.survey

--- python/pfs/ga/pipeline/data/__init__.py
+++
@@ -1,7 +0,0 @@
-from .intfilter import IntFilter
-from .hexfilter import HexFilter
-from .stringfilter import StringFilter
-from .datefilter import DateFilter
-
-from .filesystemconnector import FileSystemConnector
-from .filesystemconfig import FileSystemConfig
