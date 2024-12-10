4933eb5212e4ab3db0fe31ecd784db361bd4a739
Laszlo Dobos <dobos@jhu.edu>
2024-12-10 08:40:52 -0500

    Updated pipeline tests.

--- tests/pipeline/gapipe/test_gapipeline.py
+++ tests/pipeline/gapipe/test_gapipeline.py
@@ -1,76 +1,93 @@
 import os
 from unittest import TestCase
 
+from pfs.datamodel import *
+from pfs.ga.pfsspec.survey.repo import FileSystemRepo as PfsFileSystemRepo
+from pfs.ga.pipeline.repo import PfsFileSystemConfig
 from pfs.ga.pipeline.gapipe import GAPipeline, GAPipelineTrace
 from pfs.ga.pipeline.gapipe.config import GAPipelineConfig
+from pfs.ga.pipeline.gapipe.steps import *
 from tests.pipeline.gapipe.config.configs import *
 
 class TestGAPipeline(TestCase):
     def get_test_config(self):
         config = GAPipelineConfig()
-        config.load(TEST_CONFIG_EDR2_90006)
+        config.load(TEST_CONFIG_RUN17_10015, ignore_collisions=True)
 
-        workdir = os.path.expandvars(os.path.join('${GAPIPE_WORKDIR}', f'{config.target.objId:016x}'))
+        workdir = os.path.expandvars(os.path.join('./tmp/test/work', f'{config.target.identity.objId:016x}'))
 
         config.workdir = workdir
         config.logdir = os.path.join(workdir, 'log')
         config.figdir = os.path.join(workdir, 'fig')
         config.outdir = workdir
-
-        config.rvfit.model_grid_path = os.path.expandvars('${GAPIPE_SYNTH_GRID}')
-        config.rvfit.psf_path = os.path.expandvars('${GAPIPE_ARM_PSF}')
            
         return config
     
-    def create_test_pipeline(self, config):
+    def get_test_repo(self, config):
+        return PfsFileSystemRepo(PfsFileSystemConfig)
+        
+    def create_test_pipeline(self, config, repo):
         trace = GAPipelineTrace(config.figdir)
-        pipeline = GAPipeline(config, trace)
+        pipeline = GAPipeline(config=config, trace=trace, repo=repo)
         return pipeline
 
     def test_validate_config(self):
         config = self.get_test_config()
-        pipeline = self.create_test_pipeline(config)
+        repo = self.get_test_repo(config)
+        pipeline = self.create_test_pipeline(config, repo)
         pipeline.validate_config()
 
-    def test_start_stop_logging(self):
-        config = self.get_test_config()
-        pipeline = self.create_test_pipeline(config)
+    # TODO: these have been moved to the script from the pipeline
+    #       modify tests accordingly
 
-        pipeline._Pipeline__start_logging()
-        pipeline._Pipeline__stop_logging()
+    # def test_start_stop_logging(self):
+    #     config = self.get_test_config()
+    #     repo = self.get_test_repo(config)
+    #     pipeline = self.create_test_pipeline(config, repo)
 
-        self.assertTrue(os.path.isfile(pipeline._Pipeline__logfile))
-        os.remove(pipeline._Pipeline__logfile)
+    #     pipeline._Pipeline__start_logging()
+    #     pipeline._Pipeline__stop_logging()
+
+    #     self.assertTrue(os.path.isfile(pipeline._Pipeline__logfile))
+    #     os.remove(pipeline._Pipeline__logfile)
 
     def test_start_stop_tracing(self):
         config = self.get_test_config()
-        pipeline = self.create_test_pipeline(config)
+        repo = self.get_test_repo(config)
+        pipeline = self.create_test_pipeline(config, repo)
 
         pipeline._Pipeline__start_tracing()
         pipeline._Pipeline__stop_tracing()
 
+    def test_step_init(self):
+        config = self.get_test_config()
+        repo = self.get_test_repo(config)
+        pipeline = self.create_test_pipeline(config, repo)
+        pipeline.reset()
+
+        pipeline._Pipeline__start_tracing()
+        context = pipeline.create_context(trace=pipeline._Pipeline__trace)
+
+        InitStep().run(context)
+
+        pipeline._Pipeline__stop_tracing()
+
     def test_step_load(self):
         config = self.get_test_config()
-        pipeline = self.create_test_pipeline(config)
-        pipeline._Pipeline__start_logging()
-        
-        pipeline._GA1DPipeline__step_load()
-        
-        pipeline._Pipeline__stop_logging()
+        repo = self.get_test_repo(config)
+        pipeline = self.create_test_pipeline(config, repo)
+        pipeline.reset()
 
-        self.assertEqual(2, len(pipeline._GA1DPipeline__pfsSingle))
+        pipeline._Pipeline__start_tracing()
+        context = pipeline.create_context(trace=pipeline._Pipeline__trace)
 
-    def test_step_validate(self):
-        config = self.get_test_config()
-        pipeline = self.create_test_pipeline(config)
-        pipeline._Pipeline__start_logging()
-        
-        pipeline._GA1DPipeline__step_load()
-        pipeline._GA1DPipeline__step_validate()
-        
-        pipeline._Pipeline__stop_logging()
+        InitStep().run(context)
+        LoadStep().run(context)
+        LoadStep().validate(context)
 
-        self.assertEqual(2, len(pipeline._GA1DPipeline__pfsSingle))
+        pipeline._Pipeline__stop_tracing()
+
+        self.assertEqual(2, len(pipeline.product_cache[PfsSingle]))
 
     def test_step_vcorr(self):
         config = self.get_test_config()
