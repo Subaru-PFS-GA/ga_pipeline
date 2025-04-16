4d47540db71ed684506fa60118d7baabc84f812f
Laszlo Dobos <dobos@jhu.edu>
2025-04-16 01:50:24 -0400

    Fixed message required to log exception.

--- python/pfs/ga/pipeline/common/pipelineerror.py
+++ python/pfs/ga/pipeline/common/pipelineerror.py
@@ -2,5 +2,7 @@ class PipelineError(Exception):
     def __init__(self, message):
         super(PipelineError, self).__init__(message)
 
+        self.message = message          # Required for logging
+
     def __str__(self):
         return str(self.message)
