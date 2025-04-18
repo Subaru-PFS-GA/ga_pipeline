b866e06dde3c282f394153be69b5013334d4cf83
Laszlo Dobos <dobos@jhu.edu>
2025-04-18 03:13:05 -0400

    Use parameter file for priors.

--- python/pfs/ga/pipeline/scripts/configure/configurescript.py
+++ python/pfs/ga/pipeline/scripts/configure/configurescript.py
@@ -2,10 +2,12 @@
 
 import os
 import re
+from copy import deepcopy
 from glob import glob
 from types import SimpleNamespace
 from datetime import datetime
 import numpy as np
+import pandas as pd
 
 from pfs.datamodel import *
 from pfs.datamodel.utils import calculatePfsVisitHash, wraparoundNVisit
@@ -34,17 +36,23 @@ class ConfigureScript(PipelineScript):
     def __init__(self):
         super().__init__()
 
-        self.__dry_run = False          # Dry run mode
-        self.__top = None               # Stop after this many objects
+        self.__params = None                # Stellar parameters to derive the priors from
+        self.__params_id = '__target_idx'   # ID column of the stellar parameters to use
+        self.__dry_run = False              # Dry run mode
+        self.__top = None                   # Stop after this many objects
 
     def _add_args(self):
         self.add_arg('--config', type=str, nargs='*', required=True, help='Configuration file')
+        self.add_arg('--params', type=str, help='Path to stellar parameters file')
+        self.add_arg('--params-id', type=str, help='ID column of the stellar parameters to use')
         self.add_arg('--dry-run', action='store_true', help='Dry run mode')
         self.add_arg('--top', type=int, help='Stop after this many objects')
 
         super()._add_args()
 
     def _init_from_args(self, args):
+        self.__params = self.get_arg('params', args, self.__params)
+        self.__params_id = self.get_arg('params_id', args, self.__params_id)
         self.__dry_run = self.get_arg('dry_run', args, self.__dry_run)
         self.__top = self.get_arg('top', args, self.__top)
 
@@ -65,102 +73,175 @@ class ConfigureScript(PipelineScript):
         files = ' '.join(self.config.config_files)
         logger.info(f'Using configuration template file(s) {files}.')
 
-        identities = self.repo.find_object(groupby='objid')
-        targets = self.__create_target_config(identities)
+        # Load the stellar parameters
+        params = self.__load_params_file()
 
-        if len(targets) == 0:
+        # Find the objects matching the command-line arguments. Arguments
+        # are parsed by the repo object itself, so no need to pass them in here.
+        identities = self.repo.find_object(groupby='objid')
+
+        if len(identities) == 0:
             logger.warning('No objects found matching the filters.')
             return
+        else:
+            logger.info(f'Found {len(identities)} objects matching the filters.')
+
+        # Create the target configuration objects
+        configs, filenames = self.__create_output_configs(identities, params)
         
         # Generate the configuration file for each target
-        self.__generate_config_files(targets)
+        self.__save_config_files(configs, filenames)
 
-    def __create_target_config(self, identities):
+    def __load_params_file(self):
+        # TODO: update this if multiple files are needed or the file format changes
+        if self.__params is not None:
+            logger.info(f'Loading stellar parameters from {self.__params}.')
+            params = pd.read_feather(self.__params)
+
+            logger.info(f'Found {len(params)} entries in stellar parameter file.')
+
+            if self.__params_id not in params.columns:
+                raise ValueError(f'ID column {self.__params_id} not found in stellar parameter file.')
+
+            params = params.set_index(self.__params_id) 
+
+            return params
+        else:
+            return None
+
+    def __create_output_configs(self, identities, params=None):
+        configs = {}
+        filenames = {}
+        for objid, id in identities.items():
+            target = self.__get_target_config(objid, id)
+            config, filename = self.__get_pipeline_config(objid, target.identity)
+            config.target = target
+
+            # Look up the stellar parameters in the params file
+            if params is not None:
+                if objid in params.index:
+                    pp = params.loc[objid]
+
+                    for k in ['M_H', 'T_eff', 'log_g', 'a_M', 'rv']:
+                        k_min = f'{k}_min'
+                        k_max = f'{k}_max'
+                        k_dist = f'{k}_dist'
+                        k_dist_mean = f'{k}_dist_mean'
+                        k_dist_sigma = f'{k}_dist_sigma'
+
+                        # Limits
+
+                        if k in config.rvfit.rvfit_args and config.rvfit.rvfit_args[k] is not None:
+                            values = config.rvfit.rvfit_args[k]
+                        else:
+                            values = None
+
+                        if k in pp and pp[k] is not None and not np.isnan(pp[k]):
+                            # Constant value, overrides limits
+                            values = float(pp[k])
+                        else:
+                            if k_min in pp and pp[k_min] is not None and not np.isnan(pp[k_min]):
+                                # Lower limit
+                                if not isinstance(values, list):
+                                    values = [-np.inf, np.inf]
+                                values[0] = pp[k_min]
+
+                            if k_max in pp and pp[k_max] is not None and not np.isnan(pp[k_max]):
+                                # Upper limit
+                                if not isinstance(values, list):
+                                    values = [-np.inf, np.inf]
+                                values[1] = pp[k_max]
+
+                        if values is not None:
+                            config.rvfit.rvfit_args[k] = values
+
+                        # Distribution
+
+                        if k_dist in config.rvfit.rvfit_args and config.rvfit.rvfit_args[k_dist] is not None:
+                            dist = config.rvfit.rvfit_args[k_dist][0]
+                            dist_args = config.rvfit.rvfit_args[k_dist][1:]
+                        else:
+                            dist = None
+                            dist_args = None
+                        
+                        if k_dist in pp and pp[k_dist] is not None:
+                            if pp[k_dist] == 'uniform':
+                                dist = 'uniform'
+                                dist_args = []
+                            elif pp[k_dist] == 'normal':
+                                dist = 'normal'
+                                dist_args = [pp[k_dist_mean], pp[k_dist_sigma]]
+                            else:
+                                raise NotImplementedError()
+
+                        if dist is not None and dist_args is not None:
+                            config.rvfit.rvfit_args[k_dist] = [dist] + dist_args
+
+            configs[objid] = config
+            filenames[objid] = filename
+
+        return configs, filenames
+
+    def __get_target_config(self, objid, id):
         """
         Return the configuration section objects for each target
         """
 
-        targets = {}
-        for objid, id in identities.items():
-            targets[objid] = GATargetConfig(
-                proposalId = id.proposalId[0],
-                targetType = id.targetType[0],
-                identity = GAObjectIdentityConfig(
-                    catId = id.catId[0],
-                    tract = id.tract[0],
-                    patch = id.patch[0],
-                    objId = objid,
-                    nVisit = wraparoundNVisit(len(id.visit)),
-                    pfsVisitHash = calculatePfsVisitHash(id.visit),
-                ),
-                observations = GAObjectObservationsConfig(
-                    visit = id.visit,
-                    arms = id.arms,
-                    spectrograph = id.spectrograph,
-                    pfsDesignId = id.pfsDesignId,
-                    fiberId = id.fiberId,
-                    fiberStatus = id.fiberStatus,
-                    obstime = id.obstime,
-                    exptime = id.exptime,
-                )
+        target = GATargetConfig(
+            proposalId = id.proposalId[0],
+            targetType = id.targetType[0],
+            identity = GAObjectIdentityConfig(
+                catId = id.catId[0],
+                tract = id.tract[0],
+                patch = id.patch[0],
+                objId = objid,
+                nVisit = wraparoundNVisit(len(id.visit)),
+                pfsVisitHash = calculatePfsVisitHash(id.visit),
+            ),
+            observations = GAObjectObservationsConfig(
+                visit = id.visit,
+                arms = id.arms,
+                spectrograph = id.spectrograph,
+                pfsDesignId = id.pfsDesignId,
+                fiberId = id.fiberId,
+                fiberStatus = id.fiberStatus,
+                obstime = id.obstime,
+                exptime = id.exptime,
             )
+        )
 
-        return targets
+        return target
 
-    def __load_target_from_pfsConfig(self, target, objId, visit, pfsConfig, i, obsTime, expTime):
-        if target.proposalId is None:
-            target.proposalId = pfsConfig.proposalId[i]
-        elif target.proposalId != pfsConfig.proposalId[i]:
-            logger.warning(f'proposalId mismatch for objId {objId}: {target.proposalId} != {pfsConfig.proposalId[i]}')
-        
-        if target.targetType is None:
-            target.targetType = pfsConfig.targetType[i]
-        elif target.targetType != pfsConfig.targetType[i]:
-            logger.warning(f'targetType mismatch for objId {objId}: {target.targetType} != {pfsConfig.targetType[i]}')
+    def __get_pipeline_config(self, objid, identity, ext='.yaml'):
+        """
+        Initialze a pipeline configuration object based on the template and the target.
+        """
 
-        if target.identity.catId != pfsConfig.catId[i]:
-            logger.warning(f'catId mismatch for objId {objId}: {target.identity.catId} != {pfsConfig.catId[i]}')
+        # TODO: should we make a deep copy here?
+        config = deepcopy(self.config)
 
-        target.observations.arm[visit] = pfsConfig.arms             # TODO: Normalize order of arms?
-        target.observations.spectrograph[visit] = pfsConfig.spectrograph[i]
-        target.observations.pfsDesignId[visit] = pfsConfig.pfsDesignId
-        target.observations.fiberId[visit] = pfsConfig.fiberId[i]
-        target.observations.fiberStatus[visit] = pfsConfig.fiberStatus[i]
-        target.observations.pfiNominal[visit] = pfsConfig.pfiNominal[i]
-        target.observations.pfiCenter[visit] = pfsConfig.pfiCenter[i]
-        
-        # TODO: update this to get exact time, not just the date
-        target.observations.obsTime[visit] = obsTime
+        # Compose the directory and file names for the identity of the object
+        # The file should be written somewhere under the work directory
+        dir = self.repo.format_dir(GAPipelineConfig, identity)
+        config_file = self.repo.format_filename(GAPipelineConfig, identity)
 
-        # TODO update this once exposure time appears in the pfsConfig file
-        target.observations.expTime[visit] = expTime
+        # Name of the output pipeline configuration
+        filename = os.path.join(dir, config_file)
 
-    def __sort_target_observations_by_visit(self, target):
-        def sort_by_visit(observations, values):
-            return np.array([ values[v] for v in observations.visit ])
+        # Update config with directory names
 
-        observations = target.observations
+        # Input data directories
+        config.datadir = self.repo.get_resolved_variable('datadir')
+        config.rerundir = self.repo.get_resolved_variable('rerundir')
 
-        # Convert the dict to numpy arrays, and sort them by visit
-        idx = np.argsort(observations.visit)
-        observations.visit = np.array(observations.visit)[idx]
+        logger.debug(f'Configured data directory for object {identity}: {config.datadir}')
+        logger.debug(f'Configured rerun directory for object {identity}: {config.rerundir}')
+        logger.debug(f'Configured work directory for object {identity}: {config.workdir}')
+        logger.debug(f'Configured output directory for object {identity}: {config.outdir}')
 
-        observations.arm = sort_by_visit(observations, observations.arm)
-        observations.spectrograph = sort_by_visit(observations, observations.spectrograph)
-        observations.pfsDesignId = sort_by_visit(observations, observations.pfsDesignId)
-        observations.fiberId = sort_by_visit(observations, observations.fiberId)
-        observations.fiberStatus = sort_by_visit(observations, observations.fiberStatus)
-        observations.pfiNominal = sort_by_visit(observations, observations.pfiNominal)
-        observations.pfiCenter = sort_by_visit(observations, observations.pfiCenter)
-        observations.obsTime = sort_by_visit(observations, observations.obsTime)
-        observations.expTime = sort_by_visit(observations, observations.expTime)
-
-    def __update_target_identity(self, target):
-        # Update the identity
-        target.identity.nVisit = wraparoundNVisit(len(target.observations.visit))
-        target.identity.pfsVisitHash = calculatePfsVisitHash(target.observations.visit)
+        return config, filename
     
-    def __generate_config_files(self, targets):
+    def __save_config_files(self, configs, filenames):
         """
         Generate a config file for each of the inputs.
 
@@ -169,11 +250,8 @@ class ConfigureScript(PipelineScript):
         """
 
         q = 0
-        for objid in sorted(targets.keys()):
-            # Generate the config
-            config, filename = self._create_output_config(targets[objid])
-
-            # Save the config to a file
+        for objid in sorted(configs.keys()):
+            config, filename = configs[objid], filenames[objid]
             if not self.__dry_run:
                 logger.info(f'Saving configuration file `{filename}`.')
                 os.makedirs(os.path.dirname(filename), exist_ok=True)
@@ -185,39 +263,6 @@ class ConfigureScript(PipelineScript):
             if self.__top is not None and q >= self.__top:
                 logger.info(f'Stopping after {q} objects.')
                 break
-
-    def _create_output_config(self, target, ext='.yaml'):
-        """
-        Initialze a pipeline configuration object based on the template and the target.
-        """
-
-        # TODO: should we make a deep copy here?
-        config = self.config
-
-        # Compose the directory and file names for the identity of the object
-        # The file should be written somewhere under the work directory
-        dir = self.repo.format_dir(GAPipelineConfig, target.identity)
-        config_file = self.repo.format_filename(GAPipelineConfig, target.identity)
-
-        # Name of the output pipeline configuration
-        filename = os.path.join(dir, config_file)
-
-        # Update config with directory names
-
-        # Input data directories
-        config.datadir = self.repo.get_resolved_variable('datadir')
-        config.rerundir = self.repo.get_resolved_variable('rerundir')
-
-        logger.debug(f'Configured data directory for object {target.identity}: {config.datadir}')
-        logger.debug(f'Configured rerun directory for object {target.identity}: {config.rerundir}')
-        logger.debug(f'Configured work directory for object {target.identity}: {config.workdir}')
-        logger.debug(f'Configured output directory for object {target.identity}: {config.outdir}')
-
-        # Update the config with the ids
-
-        config.target = target
-
-        return config, filename
 
 def main():
     script = ConfigureScript()
