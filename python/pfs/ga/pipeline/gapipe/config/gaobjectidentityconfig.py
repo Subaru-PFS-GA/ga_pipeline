97609627be4eeb2440469daaf176ae6e7185814f
Laszlo Dobos <dobos@jhu.edu>
2025-07-11 12:05:44 -0400

    Fixed file name format for GAObject.

--- python/pfs/ga/pipeline/gapipe/config/gaobjectidentityconfig.py
+++ python/pfs/ga/pipeline/gapipe/config/gaobjectidentityconfig.py
@@ -24,4 +24,4 @@ class GAObjectIdentityConfig(Config):
 
     def __str__(self):
         if self.nVisit is not None and self.pfsVisitHash is not None:
-            return f'{self.catId:05d}-{self.tract:05d}-{self.patch}-{self.objId:016x}-{self.nVisit:03d}-0x{self.pfsVisitHash:016x}'
+            return f'{self.catId:05d}-{self.objId:016x}-{self.nVisit:03d}-0x{self.pfsVisitHash:016x}'
